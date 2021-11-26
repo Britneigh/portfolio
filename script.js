fa05ea602adf0717fd06ebd3eae2a468278b5caa
Britneigh <britneynguyen18@yahoo.com>
2021-11-26 22:51:14 +0000

    Added the projects

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function preview1() {
+    window.open("https://britneigh.github.io/vertical-slider/");
+}
+
+function preview2() {
+    window.open("https://britneigh.github.io/myDogsTrust/");
+}
+
+function preview3() {
+    window.open("https://britneigh.github.io/snake-game/");
+}

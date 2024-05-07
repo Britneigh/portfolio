e9ca67ebbe29ab163378491055ce5d305db19cb2
Britney Nguyen <britneynguyen18@yahoo.com>
2024-05-07 12:16:00 +0200

    Changed home layout

--- script.js
+++ script.js
@@ -3,13 +3,21 @@
 function preview1() {
     window.open("https://britneigh.github.io/vertical-slider/");
 }
-
 function preview2() {
     window.open("https://britneigh.github.io/DogsTrust/");
 }
 
 function preview3() {
     window.open("https://britneigh.github.io/snake-game/");
+}
+function preview4() {
+    window.open("https://britneigh.github.io/digital-clock/");
+}
+function preview5() {
+    window.open("https://britneigh.github.io/colour-buttons/");
+}
+function preview6() {
+    window.open("https://github.com/Britneigh/rock-paper-scissors-game");
 }
 
 //Collecting the HTML elements for use in JS
@@ -20,26 +28,23 @@ const projects = document.querySelector(".projects");
 const home = document.querySelector(".home");
 const aboutMe = document.querySelector(".aboutMe");
 const contact = document.querySelector(".contact");
-const cv = document.querySelector(".cv");
 
 //Clicking the German flag will switch the website to German
 document.querySelector("#German").onclick = function (){
-    L1.innerText = "Hallo! Wilkommen auf meiner Portfolio-Seite!";
+    L1.innerText = "Hallo! Ich heiße";
     L2.innerText = "Scrollen Sie nach unten, um meine Projekte zu sehen.";
     projects.innerText = "MEINE PROJEKTE";
     home.innerText = "Startseite";
     aboutMe.innerText = "Über mich";
     contact.innerText = "Kontakt";
-    cv.innerText = "Lebenslauf";
 }
 
 //Clicking the UK flag will switch the website to English
 document.querySelector("#English").onclick = function (){
-    L1.innerText = "Hi there, welcome to my Portfolio page!";
-    L2.innerText = "Scroll down to take a look at my projects";
+    L1.innerText = "Hi! I am";
+    L2.innerText = "Scroll down to take a look at some of my projects";
     projects.innerText = "MY PROJECTS";
     home.innerText = "Home";
     aboutMe.innerText = "About Me";
     contact.innerText = "Contact";
-    cv.innerText = "CV";
 }
